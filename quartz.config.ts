0ae62acf4ca7b84dd153dd336bced5422990895d
andreortizrijo <andreortizrijo@gmail.com>
2025-02-02 19:27:54 +0000

    Quartz sync: Feb 2, 2025, 7:27 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "andreortizrijo.github.io",
+    baseUrl: "andreortizrijo.github.io/myjourney/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,
